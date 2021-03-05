538126aa755d7249b2bb409c839773dda8341ce5
Yann Rannou <yann.rannou777@gmail.com>
2021-03-05 04:12:27 +0100

    Last Push

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,11 +3,29 @@ class EmailsController < ApplicationController
     @emails = Email.all
   end
   def create
-    @email = Email.create(email_params)
-
-    respond_to do |format|
-      format.html { redirect_to emails_path }
-      format.js { }
-    end
+    @email = Email.create(sender: Faker::Internet.email, text: Faker::Quote.matz)
   end
+
+  def show
+    @email.read = true
+    @email.save
+	end
+
+#destroy the params given email
+  def destroy
+    @email = Email.destroy(params[:id])
+    redirect_to root_path   
+  end
+
+
+#declare var that will be used in 4 functions
+  private 
+  def all_emails
+      @emails = Email.all
+    end
+
+    def set_emails
+      @email = Email.find(params[:id])
+		end
+
 end
